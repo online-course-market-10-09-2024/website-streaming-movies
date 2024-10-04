6c91b62b3f51fae8c0487b2cf988ea69d69ec35e
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-04 18:54:56 +0700

    Init nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+  in {
+    devShells = forAllSystems (system:
+      let
+        pkgs = nixpkgsFor.${system}; # Interpolation được sửa
+      in {
+        default = pkgs.mkShell {
+          nativeBuildInputs = [
+            # Development tools
+            pkgs.dbeaver-bin
+            pkgs.nodejs
+            pkgs.go
+
+            # Language servers
+            pkgs.gopls
+            pkgs.nixd
+
+            # Other tools
+            pkgs.scc
+          ];
+        };
+      });
+  };
+}
