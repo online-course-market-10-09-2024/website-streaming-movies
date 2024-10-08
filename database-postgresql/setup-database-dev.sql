0a2e485d9abfda9c12e79e639e7f5db6b769b9b3
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-08 18:37:08 +0700

    Add movie_category functions and insert test values

--- database-postgresql/setup-database-dev.sql
+++ database-postgresql/setup-database-dev.sql
@@ -5,3 +5,9 @@ CREATE DATABASE streamingmovies;
 -- Create tables and types
 \i /docker-entrypoint-initdb.d/src/enums.sql
 \i /docker-entrypoint-initdb.d/src/data_tables.sql
+
+-- Create category_movie functions
+\i /docker-entrypoint-initdb.d/src/functions/movie_category.sql
+
+-- Insert test
+\i /docker-entrypoint-initdb.d/src/insert-test.sql
---
+++ database-postgresql/src/insert-test.sql
@@ -0,0 +1,3 @@
+INSERT INTO movie_category (name) VALUES ('category 1');
+INSERT INTO movie_category (name) VALUES ('category 2');
+INSERT INTO movie_category (name) VALUES ('category 11');
