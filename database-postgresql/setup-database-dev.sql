e8148d4316474efe7741175e0b63964fa2e58017
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-09 06:07:23 +0700

    Update database-postgresql fix movie_category functions

--- database-postgresql/setup-database-dev.sql
+++ database-postgresql/setup-database-dev.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE streamingmovies;
-
-\c streamingmovies;
-
 -- Create tables and types
 \i /docker-entrypoint-initdb.d/src/enums.sql
 \i /docker-entrypoint-initdb.d/src/data_tables.sql
