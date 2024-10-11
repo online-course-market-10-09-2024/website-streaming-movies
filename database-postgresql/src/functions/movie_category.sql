7ce373316c6e4a03c51780a95712a1c5eca4c74b
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-11 21:28:18 +0700

    Add database-postgresql get_all_movie_category function and insert test values

--- database-postgresql/src/functions/movie_category.sql
+++ database-postgresql/src/functions/movie_category.sql
@@ -71,3 +71,53 @@ CREATE OR REPLACE FUNCTION count_movie_category()
         RETURN value_count;
     END;
     $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_all_movie_category(
+        input_search TEXT,
+        input_limit  NUMERIC,
+        input_page   NUMERIC
+    )
+    RETURNS TABLE (
+        id   UUID,
+        name TEXT
+    )
+    AS $$
+    DECLARE
+        value_count NUMERIC;
+        max_page    NUMERIC;
+    BEGIN
+        -- Get count
+        SELECT count_movie_category() INTO value_count;
+
+        -- Set default limit range
+        IF input_limit < 10 THEN
+            input_limit := 10;
+        ELSIF input_limit > 50 THEN
+            input_limit := 50;
+        END IF;
+
+        -- Set max_page
+        max_page :=
+            CASE WHEN CEIL(value_count::NUMERIC / input_limit) > 0
+                THEN CEIL(value_count::NUMERIC / input_limit) - 1
+                ELSE 0
+            END;
+
+
+        RAISE NOTICE 'Max Page: %', max_page;
+
+        -- Set default page range
+        IF input_page < 1 THEN
+            input_page := 0;
+        ELSIF input_page > max_page THEN
+            input_page := max_page;
+        END IF;
+
+        RETURN QUERY
+            SELECT movie_category.id, movie_category.name
+            FROM movie_category
+            WHERE movie_category.name ILIKE '%' || input_search || '%'
+            LIMIT input_limit
+            OFFSET input_limit*input_page;
+    END;
+    $$ LANGUAGE plpgsql;
