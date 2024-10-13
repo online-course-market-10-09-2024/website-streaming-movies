77a4c76bee616b7f776dabb76c9eae5ac5fc4973
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-13 11:21:05 +0700

    Update web-next config mjs to ts

---
+++ web-next/next.config.ts
@@ -0,0 +1,10 @@
+import { NextConfig } from 'next';
+
+/** @type {NextConfig} */
+const nextConfig: NextConfig = {
+    images: {
+        domains: ['robohash.org', 'c4.wallpaperflare.com', 'cdn.prod.website-files.com'],
+    },
+};
+
+export default nextConfig;
---
+++ web-next/postcss.config.ts
@@ -0,0 +1,10 @@
+import { Config } from 'postcss-load-config';
+
+/** @type {Config} */
+const config: Config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;
